1d1d61333672be868e295bc97ed886749ba6bc63
Jan Jongboom <janjongboom@gmail.com>
2024-11-22 15:15:14 +0100

    Rework testing to include JojoDiff binary deployments

--- integration-tests/run-tests.js
+++ integration-tests/run-tests.js
@@ -1,72 +1,144 @@
 const fs = require('fs');
 const Path = require('path');
-const execSync = require('child_process').execSync;
+const spawn = require('child_process').spawn;
 const crypto = require('crypto');
+const util = require('util');
 
-let jdiff = 'jdiff';
-if (process.argv.indexOf('--jdiff-path') > -1) {
-    jdiff = process.argv[process.argv.indexOf('--jdiff-path') + 1];
-}
+(async () => {
+    try {
+        let jdiff = 'jdiff';
+        if (process.argv.indexOf('--jdiff-path') > -1) {
+            jdiff = process.argv[process.argv.indexOf('--jdiff-path') + 1];
+        }
 
-const files = fs.readdirSync(Path.join(__dirname, 'source'));
-if (!fs.existsSync(Path.join(__dirname, 'target'))) {
-    fs.mkdirSync(Path.join(__dirname, 'target'));
-}
+        const files = await fs.promises.readdir(Path.join(__dirname, 'source'));
+        if (!await pathExists(Path.join(__dirname, 'target'))) {
+            await fs.promises.mkdir(Path.join(__dirname, 'target'), { recursive: true });
+        }
 
-const target_files = [];
-for (let s = 0; s < files.length; s++) {
-    for (let t = 0; t < files.length; t++) {
-        if (t === s) continue;
+        const target_files = [];
+        for (let s = 0; s < files.length; s++) {
+            for (let t = 0; t < files.length; t++) {
+                if (t === s) continue;
 
-        target_files.push([
-            Path.join(__dirname, 'source', files[s]),
-            Path.join(__dirname, 'source', files[t]),
-            Path.join(__dirname, 'target',
-                Path.basename(files[s], Path.extname(files[s])) +
-                '_' +
-                Path.basename(files[t], Path.extname(files[t])) + '.diff'),
-            crypto.createHash('sha256').update(fs.readFileSync(Path.join(__dirname, 'source', files[t]))).digest('hex')
-        ]);
+                target_files.push([
+                    Path.join(__dirname, 'source', files[s]),
+                    Path.join(__dirname, 'source', files[t]),
+                    Path.join(__dirname, 'target',
+                        Path.basename(files[s], Path.extname(files[s])) +
+                        '_' +
+                        Path.basename(files[t], Path.extname(files[t])) + '.diff'),
+                    crypto.createHash('sha256').update(await fs.promises.readFile(Path.join(__dirname, 'source', files[t]))).digest('hex')
+                ]);
+            }
+        }
+
+        console.log('Generating diffs...');
+
+        for (let f of target_files) {
+            let [ sourceFile, targetFile, diffFile ] = f;
+            await spawnHelper(jdiff, [
+                sourceFile,
+                targetFile,
+                diffFile
+            ], {
+                acceptableExitCodes: [ 0, 1, 2 ], // all of these can be returned by jdiff
+            });
+
+            console.log('Generated', diffFile);
+        }
+
+        console.log('\nPatching...\n');
+
+        let total_time = 0;
+        let total_ok = 0;
+        let total_failed = 0;
+
+        for (let f of target_files) {
+            let [ sourceFile, targetFile, diffFile, hash ] = f;
+            let patched = diffFile.replace(/\.diff$/, '.patched');
+
+            let start = Date.now();
+            await spawnHelper(Path.join(__dirname, '..', 'janpatch-cli'), [
+                sourceFile,
+                diffFile,
+                patched,
+            ]);
+            let elapsed = '(' + ((Date.now() - start)) + ' ms.)';
+
+            total_time += (Date.now() - start);
+
+            let newHash = crypto.createHash('sha256').update(await fs.promises.readFile(patched)).digest('hex');
+
+            let name = Path.basename(patched, '.patched');
+
+            if (newHash !== hash) {
+                console.log('    \u2717', name, 'Hash mismatch (expected: ' + hash + ', but was ' + newHash + ')', elapsed);
+                total_failed++;
+            }
+            else {
+                console.log('    \u2714', name, 'OK', elapsed);
+                total_ok++;
+            }
+        }
+
+        console.log(`\nSummary: ${total_ok}/${total_ok+total_failed} succeeded (total time: ${total_time} ms.)`);
     }
-}
-
-console.log('Generating diffs...');
-
-for (let f of target_files) {
-    let [ sourceFile, targetFile, diffFile ] = f;
-    execSync(`${jdiff} "${sourceFile}" "${targetFile}" "${diffFile}"`);
-
-    console.log('Generated', diffFile);
-}
-
-console.log('\nPatching...\n');
-
-let total_time = 0;
-let total_ok = 0;
-let total_failed = 0;
-
-for (let f of target_files) {
-    let [ sourceFile, targetFile, diffFile, hash ] = f;
-    let patched = diffFile.replace(/\.diff$/, '.patched');
-
-    let start = Date.now();
-    execSync(`${Path.join(__dirname, '..', 'janpatch-cli')} "${sourceFile}" "${diffFile}" "${patched}"`);
-    let elapsed = '(' + ((Date.now() - start)) + ' ms.)';
-
-    total_time += (Date.now() - start);
-
-    let newHash = crypto.createHash('sha256').update(fs.readFileSync(patched)).digest('hex');
-
-    let name = Path.basename(patched, '.patched');
-
-    if (newHash !== hash) {
-        console.log('    \u2717', name, 'Hash mismatch (expected: ' + hash + ', but was ' + newHash + ')', elapsed);
-        total_failed++;
+    catch (ex) {
+        console.error(`Failed to run integration tests`, ex);
+        process.exit(1);
     }
-    else {
-        console.log('    \u2714', name, 'OK', elapsed);
-        total_ok++;
+})();
+
+function spawnHelper(command, args, opts) {
+    opts = opts || { };
+    if (typeof opts.ignoreErrors !== 'boolean') {
+        opts.ignoreErrors = false;
     }
+    if (typeof opts.acceptableExitCodes === 'undefined') {
+        opts.acceptableExitCodes = [ 0 ];
+    }
+
+    return new Promise((resolve, reject) => {
+        // console.log(`spawning ${command} ${args.join(' ')}`);
+        const p = spawn(command, args, { env: process.env, cwd: opts.cwd });
+
+        const allData = [];
+
+        p.stdout.on('data', (data) => {
+            allData.push(data);
+            // process.stdout.write(data);
+        });
+
+        p.stderr.on('data', (data) => {
+            allData.push(data);
+            // process.stderr.write(data);
+        });
+
+        p.on('error', reject);
+
+        p.on('close', (code) => {
+            if (opts.acceptableExitCodes.indexOf(code) > -1 || opts.ignoreErrors === true) {
+                resolve(Buffer.concat(allData).toString('utf-8'));
+            }
+            else {
+                let errorCodeStr = opts.acceptableExitCodes.length === 1 ?
+                    `Error code was not ${opts.acceptableExitCodes[0]}` :
+                    `Error code was not in ${JSON.stringify(opts.acceptableExitCodes)}`;
+                reject(`${errorCodeStr} (but ${code}): ` + Buffer.concat(allData).toString('utf-8'));
+            }
+        });
+    });
 }
 
-console.log(`\nSummary: ${total_ok}/${total_ok+total_failed} succeeded (total time: ${total_time} ms.)`);
+async function pathExists(path) {
+    let exists = false;
+    try {
+        await util.promisify(fs.stat)(path);
+        exists = true;
+    }
+    catch (ex) {
+        /* noop */
+    }
+    return exists;
+}
---
+++ jdiff-binary-sources/build-all.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+set -e
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    SEDCMD="sed -i '' -e"
+    LC_CTYPE=C
+    LANG=C
+else
+    SEDCMD="sed -i -e"
+fi
+
+rm -rf $SCRIPTPATH/jdiff07/
+rm -rf $SCRIPTPATH/jdiff081/
+rm -rf $SCRIPTPATH/jdiff085/
+
+echo "Extracting jdiff07..."
+cd $SCRIPTPATH
+rm -rf jdiff07
+mkdir -p jdiff07
+tar -xzf jdiff07.tgz -C jdiff07
+mv jdiff07/jojodiff07/* jdiff07
+rm -r jdiff07/jojodiff07/
+echo "Extracting jdiff07 OK"
+echo ""
+
+echo "Extracting jdiff081..."
+cd $SCRIPTPATH
+rm -rf jdiff081
+mkdir -p jdiff081
+tar -xzf jdiff081.tgz -C jdiff081
+mv jdiff081/jdiff081/* jdiff081
+rm -r jdiff081/jdiff081/
+echo "Extracting jdiff081 OK"
+echo ""
+
+echo "Extracting jdiff085..."
+cd $SCRIPTPATH
+rm -rf jdiff085
+mkdir -p jdiff085
+tar -xzf jdiff085.tgz -C jdiff085
+mv jdiff085/jdiff085/* jdiff085
+rm -r jdiff085/jdiff085/
+echo "Extracting jdiff085 OK"
+echo ""
+
+echo "Building jdiff07..."
+cd $SCRIPTPATH/jdiff07/src
+$SEDCMD "s/GCC4=gcc-4/GCC4=gcc/g" Makefile      # patch out gcc-4
+make -j`nproc`
+mv jdiff.exe jdiff
+if [ -f "Makefile''" ]; then
+    rm "Makefile''"
+fi
+echo "Building jdiff07 OK"
+echo ""
+
+echo "Building jdiff081..."
+cd $SCRIPTPATH/jdiff081/src
+$SEDCMD "s/#define ulong unsigned long int//g" JDefs.h      # patch out unused ulong define
+make -j`nproc`
+echo "Building jdiff081 OK"
+echo ""
+
+echo "Building jdiff085..."
+cd $SCRIPTPATH/jdiff085/src
+$SEDCMD "s/-m64//g" Makefile      # patch out -m64
+make -j`nproc`
+echo "Building jdiff085 OK"
+echo ""
